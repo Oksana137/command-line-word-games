2afb18ec9b58c6825223e4b9c3ef9a9b74786aa7
Oksana <axana13@gmail.com>
2024-08-07 08:27:42 +0200

    rock paper scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,6 @@
+// node rockPaperScissors.js rock
+// # Output: You chose rock. Computer chose scissors. You win!
+
 const args = process.argv.slice(2);
 
 if (args.length !== 1) {
@@ -5,9 +8,39 @@ if (args.length !== 1) {
   return;
 }
 
-const userInput = args[0];
+const userChoice = args[0];
 const validChoices = ["rock", "paper", "scissors"];
 
-if (!validChoices.includes(userInput)) {
+if (!validChoices.includes(userChoice)) {
   console.log("Invalid choice. Please choose rock, paper, or scissors.");
 }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const computerChoice = validChoices[getRandomInt(3)];
+
+function who_wins(userChoice, computerChoice) {
+  if (userChoice == computerChoice) {
+    return "Draw";
+  }
+
+  switch (userChoice) {
+    case "paper":
+      return computerChoice == "paper" ? "You win" : "You lose";
+      break;
+    case "rock":
+      return computerChoice == "scissors" ? "You win" : "You lose";
+      break;
+    case "scissors":
+      return computerChoice == "paper" ? "You win" : "You lose";
+      break;
+  }
+}
+
+const result = who_wins(userChoice, computerChoice);
+
+console.log(
+  `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}!`
+);
