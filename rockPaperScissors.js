74b37d2613bc738fde348332a7176e18f8415485
Oksana <axana13@gmail.com>
2024-08-08 14:51:34 +0200

    better

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,34 +1,20 @@
 // node rockPaperScissors.js rock
 // # Output: You chose rock. Computer chose scissors. You win!
 
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-  console.error("Please provide exactly rock or paper or scissors");
-  return;
-}
-
-const userChoice = args[0];
 const validChoices = ["rock", "paper", "scissors"];
-
-if (!validChoices.includes(userChoice)) {
-  console.log("Invalid choice. Please choose rock, paper, or scissors.");
-}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const computerChoice = validChoices[getRandomInt(3)];
-
-function who_wins(userChoice, computerChoice) {
+function determineWinner(userChoice, computerChoice) {
   if (userChoice == computerChoice) {
     return "Draw";
   }
 
   switch (userChoice) {
     case "paper":
-      return computerChoice == "paper" ? "You win" : "You lose"
+      return computerChoice == "paper" ? "You win" : "You lose";
     case "rock":
       return computerChoice == "scissors" ? "You win" : "You lose";
     case "scissors":
@@ -36,8 +22,27 @@ function who_wins(userChoice, computerChoice) {
   }
 }
 
-const result = who_wins(userChoice, computerChoice);
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length !== 1) {
+    console.error("Please provide exactly rock or paper or scissors");
+    process.exit(1);
+  }
 
-console.log(
-  `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}!`
-);
+  const userChoice = args[0];
+
+  if (!validChoices.includes(userChoice)) {
+    console.log("Invalid choice. Please choose rock, paper, or scissors.");
+    process.exit(1);
+  }
+
+  const computerChoice = validChoices[getRandomInt(3)];
+
+  const result = determineWinner(userChoice, computerChoice);
+
+  console.log(
+    `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}!`
+  );
+}
+
+main();
